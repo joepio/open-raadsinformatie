9dc8b556f924465885568adeab2bf3a328d95e9b
Breyten Ernsting <bje@dds.nl>
2019-02-18 15:48:39 +0100

    Revert "Oops."
    This reverts commit 681b5894f3c79f4480a1e1ae9fed6cb51c6014ab.

--- ocd_backend/models/definitions/schema.py
+++ ocd_backend/models/definitions/schema.py
@@ -24,7 +24,7 @@ class MediaObject(Schema, owl.Thing):
     date_modified = DateTimeProperty(Schema, 'dateModified')
     original_url = StringProperty(Schema, 'isBasedOn')
     text = StringProperty(Schema, 'text')
-    classifications = Relation(Schema, 'PropertyValue')
+    classifications = Relation(Schema, 'classifications')
 
     enricher_task = 'file_to_text'
 
